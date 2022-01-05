2c4e3bc8dbd62c2140a1222502539e6f1fafa5d1
ZephirSD <siegfieddallery@gmail.com>
2022-01-05 16:00:16 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text("Je suis mega riche"),
+        ),
+        body: SafeArea(
+          child:
+        )
+          
+        )),
+  ));
+}
