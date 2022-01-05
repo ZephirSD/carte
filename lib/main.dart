7a2e726c505a37d068f4eb86f1139042d370d503
ZephirSD <siegfieddallery@gmail.com>
2022-01-05 18:00:32 +0400

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,80 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: const Text("Je suis mega riche"),
-        ),
-        body: SafeArea(
-          child:
-        )
-          
-        )),
-  ));
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+            body: Center(
+                child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1640112764053-0e0c7120e7f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=726&q=80'),
+                  radius: 60,
+                ),
+                Text(
+                  'Siegfried DALLERY',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(7),
+                      )),
+                  //color: Colors.white,
+                  width: double.infinity,
+                  margin: EdgeInsets.fromLTRB(25, 20, 25, 0),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Column(
+                    children: [
+                      Text(
+                        '0692 ** ** **',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal.shade100,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(7),
+                      )),
+                  //color: Colors.white,
+                  width: double.infinity,
+                  margin: EdgeInsets.fromLTRB(25, 20, 25, 0),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Column(
+                    children: [
+                      Text(
+                        'test@gmail.com',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal.shade100,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )))),
+  );
 }
