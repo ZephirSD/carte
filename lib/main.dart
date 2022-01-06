65d6262482b711909988f1b1d8a0ef95b88a63f5
ZephirSD <siegfrieddallery@gmail.com>
2022-01-06 09:53:44 +0400

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
                 CircleAvatar(
                   backgroundImage: NetworkImage(
                       'https://images.unsplash.com/photo-1640112764053-0e0c7120e7f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=726&q=80'),
-                  radius: 60,
+                  radius: 65,
                 ),
                 Text(
                   'Siegfried DALLERY',
