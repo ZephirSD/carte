f268d5429e148e097f36de758f5a8443a4ece28f
ZephirSD <siegfieddallery@gmail.com>
2022-01-10 09:12:01 +0400

    Ajout Padding Icon

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ void main() {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 CircleAvatar(
-                  backgroundImage: NetworkImage('https://images.unsplash.com/photo-1640112764053-0e0c7120e7f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=726&q=80'),
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1640112764053-0e0c7120e7f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=726&q=80'),
                   radius: 60,
                 ),
                 Text(
@@ -51,10 +52,6 @@ void main() {
                           color: Colors.teal.shade100,
                           size: 30,
                         ),
-                      Icon(
-                        Icons.phone_sharp,
-                        color: Colors.teal.shade100,
-                        size: 30,
                       ),
                       Text(
                         '0692 ** ** **',
@@ -87,10 +84,6 @@ void main() {
                           color: Colors.teal.shade100,
                           size: 30,
                         ),
-                      Icon(
-                        Icons.email_rounded,
-                        color: Colors.teal.shade100,
-                        size: 30,
                       ),
                       Text(
                         'test@gmail.com',
